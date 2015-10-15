0c80af3274d7c39d1ef5f1fa11dfd9b33545014d
Andres Molina <andresmag2@gmail.com>
2015-10-15 14:07:04 +0200

    Update Napakalaki.java

--- Napakalaki/src/napakalaki/Napakalaki.java
+++ Napakalaki/src/napakalaki/Napakalaki.java
@@ -22,12 +22,17 @@ public class Napakalaki {
         // Array contenedor de monstruos
         ArrayList<Monster> monstruos = new ArrayList();
         
+        //Declaración de los distintos tipos de monstruos con sus malos rollos y premios y añadidos al vector
+        //Objetos necesarios
         BadConsequence badConsequence;
+        Prize prize;
+        
+        //Monstruos
         badConsequence = new BadConsequence("Pierdes tu armadura visible y otra oculta",
                 0,
                 new ArrayList(Arrays.asList(TreasureKind.ARMOR)),
                 new ArrayList(Arrays.asList(TreasureKind.ARMOR)));
-        Prize prize = new Prize(1, 2);
+        prize = new Prize(1, 2);
         monstruos.add(new Monster("3 Byakhees de bonanza", 8, badConsequence, prize));
         
         badConsequence = new BadConsequence("Embobados con el lindo primigenio te descartas de tu casco visible",
@@ -36,6 +41,8 @@ public class Napakalaki {
                 new ArrayList());
         prize = new Prize(1, 1);
         monstruos.add(new Monster("Chibithulhu" , 2, badConsequence, prize));
+        
+        //Prueba de muestra por pantalla
         System.out.println(monstruos.get(0).toString() );
         System.out.println(monstruos.get(1).toString() );
         
