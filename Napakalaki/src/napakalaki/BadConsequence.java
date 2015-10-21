6f5bf0549a4175efe040e485fae807bdd334c4a7
AlgebroHeavyGuns <AlgebroHeavyGuns@users.noreply.github.com>
2015-10-21 15:37:36 +0200

    Update BadConsequence.java
    He puesto un poco más ordenado el metodo toString(). Es meramente estético, no he cambiado codigo, salvo quitar los '+=' y empalmar
    con '+' las diferentes partes.

--- Napakalaki/src/napakalaki/BadConsequence.java
+++ Napakalaki/src/napakalaki/BadConsequence.java
@@ -70,11 +70,12 @@ public class BadConsequence {
     public String toString(){
         String borraCorchetesVisible = specificVisibleTreasures.toString();
         String borraCorchetesOculto = specificHiddenTreasures.toString();
-        String resp = "\nTexto: \"" + text + "\" \nLevels = " + Integer.toString(levels) + "\tTesoros: " + " visbles = " + Integer.toString(nVisibleTreasures + specificVisibleTreasures.size())
-                + " ocultos = " + Integer.toString(nHiddenTreasures + specificHiddenTreasures.size());
-        resp += "\nVisibles : " + borraCorchetesVisible.substring(1, borraCorchetesVisible.length()-1)
-                + "\nOcultos : " + borraCorchetesOculto.substring(1, borraCorchetesOculto.length()-1);
-        resp += "\nMuerte : ";
+        String resp = "\nTexto: \"" + text + "\" \nLevels = " + Integer.toString(levels) + "\tTesoros: "
+                + " visbles = " + Integer.toString(nVisibleTreasures + specificVisibleTreasures.size())
+                + " ocultos = " + Integer.toString(nHiddenTreasures + specificHiddenTreasures.size())
+                + "\nVisibles : " + borraCorchetesVisible.substring(1, borraCorchetesVisible.length()-1)
+                + "\nOcultos : " + borraCorchetesOculto.substring(1, borraCorchetesOculto.length()-1)
+                + "\nMuerte : "
         if(death)
             resp+= "Sí";
         else
