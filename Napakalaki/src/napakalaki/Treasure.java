a7c6d319586db1f881d54a525ed4aa19d829db21
AlgebroHeavyGuns <AlgebroHeavyGuns@users.noreply.github.com>
2015-11-05 10:03:07 +0100

    Create Treasure.java
    No está terminado.

---
+++ Napakalaki/src/napakalaki/Treasure.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package napakalaki;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Treasure {
+    private TreasureKind type;
+    private String name;
+    private int bonus;
+    
+    public Treasure (String n, int b, TreasureKind t){
+        type = t;
+        name  = n;
+        bonus = b;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public int getBonus() {
+        return bonus;
+    }
+   
+    public TreasureKind getType(){
+        return type;
+    }
+    
+}
